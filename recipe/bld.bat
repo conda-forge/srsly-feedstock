0d960197f431f4103cb9c3f3ac7aeedf94a714be
Shaun Walbridge <shaun.walbridge@gmail.com>
2025-02-26 08:59:33 -0500

    fix paths on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,9 @@
 :: remove vendored copies of cloudpickle and ujson, 
 :: these are now runtime dependencies of the package.
-rd /q /s "%SRC_DIR%\srsly\srsly\cloudpickle"
-rd /q /s "%SRC_DIR%\srsly\srsly\ujson"
-rd /q /s "%SRC_DIR%\srsly\srsly\tests\cloudpickle"
-rd /q /s "%SRC_DIR%\srsly\srsly\tests\ujson"
+rd /q /s "%SRC_DIR%\srsly\cloudpickle"
+rd /q /s "%SRC_DIR%\srsly\ujson"
+rd /q /s "%SRC_DIR%\srsly\tests\cloudpickle"
+rd /q /s "%SRC_DIR%\srsly\tests\ujson"
 if errorlevel 1 exit 1
 
 "%PYTHON%" -m pip install . --no-deps -vv
